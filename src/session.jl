65f952e40171a74bc8f07be1f39cfad24b283082
Yoga14s\CXIN <genius_cx@qq.com>
2022-05-07 19:44:42 +0800

    bug fix

--- src/session.jl
+++ src/session.jl
@@ -1,19 +1,19 @@
 mutable struct RPCSession{S}
     sock::S
     msgid::Int
-    RPCSession(addr::IPAddr, port::Integer) = new(connect(addr, port), 1)
-    RPCSession(port::Integer) = new(connect(port), 1)
 end
+
+RPCSession(port::Integer) = RPCSession(connect(port), 1)
+RPCSession(addr::IPAddr, port::Integer) = RPCSession(connect(addr, port), 1)
 
 Base.close(s::RPCSession) = close(s.sock)
 
-function call(session, method, params...; timeout=0, interval=0.001)
+function call(session, method, params...; timeout=0.001, interval=0.001)
     msgid = session.msgid
     session.msgid += 1
     msg = pack([REQUEST, msgid, method, params])
     write(session.sock, msg)
-    res = recvbytes(session.sock; timeout, interval) |> unpack
-    isnothing(res) && return nothing
+    res = unpack(readavailable(session.sock))
     # [1, msgid, error, result]
     @inbounds if res[1] != 1 || res[2] != msgid || !isnothing(res[3])
         # TODO throw error?
@@ -23,17 +23,21 @@ function call(session, method, params...; timeout=0, interval=0.001)
     end
 end
 
-function recvbytes(sock; timeout=0, interval=0.001)
+function recvbytes(sock; timeout=0.001, interval=0.001)
     res = nothing
-    t = @async res = readavailable(sock)
-    times = div(timeout, interval)
+    ch = Channel{Bool}(1)
+    t = @async begin
+        isready(ch) && return
+        res = readavailable(sock)
+    end
+    times = Int(timeout / interval)
     while times > 0
         sleep(interval)
-        !isnothing(res) && return res
+        istaskdone(t) && return res
         times -= 1
     end
-    isnothing(res) && Base.throwto(t, InterruptException())
-    nothing
+    put!(ch, true)
+    return res
 end
 
 
